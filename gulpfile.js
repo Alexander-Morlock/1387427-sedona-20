f685a9e958813331caf2ef5f8329e76305a35167
Alexander-Morlock <artofdesign@inbox.ru>
2020-08-23 23:52:08 +0200

    7-1

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
 
+const webp = require("gulp-webp");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+
 // Styles
 
 const styles = () => {
@@ -16,8 +23,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+	.pipe(csso())
+	.pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -28,7 +37,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -46,6 +55,71 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-exports.default = gulp.series(
-  styles, server, watcher
+
+// Gulp WebP
+
+gulp.task('webp', () =>
+    gulp.src('source/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(gulp.dest("source/img"))
 );
+
+// Images optimization
+
+const images = () => {
+return gulp.src("source/img/**/*.{jpg,png,svg}")
+.pipe(imagemin([
+imagemin.optipng({optimizationLevel: 3}),
+imagemin.mozjpeg({progressive: true}),
+imagemin.svgo()
+]))
+}
+exports.images = images;
+
+// SVG Sprite
+
+const sprite = () => {
+return gulp.src("source/img/**/icon-*.svg")
+.pipe(svgstore())
+.pipe(rename("sprite.svg"))
+.pipe(gulp.dest("build/img"))
+}
+exports.sprite = sprite;
+
+// Production
+
+const copy = () => {
+return gulp.src([
+"source/fonts/**/*.{woff,woff2}",
+"source/img/**",
+"source/js/**",
+"source/*.ico"
+], {
+base: "source"
+})
+.pipe(gulp.dest("build"));
+};
+exports.copy = copy;
+
+// Clean «Build» folder
+
+const clean = () => {
+return del("build");
+};
+exports.clean = clean;
+
+// Default
+exports.default = gulp.series(
+  styles, server, watcher, sprite
+);
+
+// Build
+
+const build = () => gulp.series(
+"clean",
+"copy",
+"css",
+"sprite",
+"html"
+);
+exports.build = build;
